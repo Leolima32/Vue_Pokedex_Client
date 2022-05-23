f5ab3346a20e0ad25df5b51e5ad8b346408fceed
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2022-05-23 11:22:35 -0300

    nav bar component

---
+++ pokedex/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="navbar is-black" role="navigation" aria-label="main navigation">
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <router-link to="/">Pokedex</router-link>                
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
